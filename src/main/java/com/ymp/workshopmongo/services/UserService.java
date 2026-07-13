a9bcdac26089efa82a858070e8e3272f267359bf
YagoMP88 <yagomartins013@gmail.com>
2026-07-13 01:31:17 -0300

    Inserção de usuário com POST

--- src/main/java/com/ymp/workshopmongo/services/UserService.java
+++ src/main/java/com/ymp/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ymp.workshopmongo.domain.User;
+import com.ymp.workshopmongo.dto.UserDTO;
 import com.ymp.workshopmongo.repository.UserRepository;
 import com.ymp.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 	
 }
